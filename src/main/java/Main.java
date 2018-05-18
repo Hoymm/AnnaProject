66934117b40e8adc2592711ff7df9370710b9618
Damian <damian.andrzej.muca@gmail.com>
2018-05-18 23:18:10 +0200

    Fix Main.class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,23 @@
 import figury.Figura;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-
     public static void main(String[] args) {
-        List <Figura> listaFigur = stwórzListęZFigurami();
+        ArrayList <Figura> listaFigur = stwórzListęZFigurami();
         wypiszFigury(listaFigur);
     }
 
-    private static List<Figura> stwórzListęZFigurami() {
-        List<Figura> listaFigur = new ArrayList<>();
+    private static ArrayList<Figura> stwórzListęZFigurami() {
+        ArrayList<Figura> listaFigur = new ArrayList<>();
         listaFigur.add(new figury.Prostokąt(2, 4));
         listaFigur.add(new figury.Kwadrat(3.5));
         listaFigur.add(new figury.Okrąg(10));
         return listaFigur;
     }
-
-    private static void wypiszFigury(List<Figura> listaFigur) {
+    private static void wypiszFigury(ArrayList<Figura> listaFigur) {
         for (Figura figura : listaFigur){
+            System.out.println(figura.getRodzaj() + " pole " + figura.getPole() + ", obwód: " + figura.getObwód());
             System.out.printf("%s pole %4.2f, obwód: %4.2f\n", figura.getRodzaj(), figura.getPole(), figura.getObwód());
         }
     }
-
 }
